992e709cd8f4e0f93c4b1d147e06391b0638b81d
Mahbub-web-hero-1997 <mahbub.programing.hero@gmail.com>
2025-08-13 16:20:33 +0600

    create user model and create user register route

---
+++ src/lib/db.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const MONGODB_URL = process.env.MONGODB_URL;
+
+if (!MONGODB_URL) {
+  throw new Error(
+    "Please define the MONGODB_URL environment variable inside .env"
+  );
+}
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { connection: null, promise: null };
+}
+
+export const connectToDatabase = async () => {
+  if (cached.connection) {
+    return cached.connection;
+  }
+  if (!cached.promise) {
+    mongoose.connect(MONGODB_URL, {}).then(() => mongoose.connection);
+  }
+  try {
+    cached.connection = await cached.promise;
+  } catch (error) {
+    cached.promise = null;
+    throw error;
+  }
+  return cached.connection;
+};
